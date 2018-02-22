70b91b9ae605df9f719484d25abbefc26bab163d
unknown <StanHelsing54@yandex.ru>
2018-02-22 14:35:39 +0700

    message

---
+++ trpo/trpo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trpo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld!");
+            Console.WriteLine("(._. ')");
+            Console.ReadKey();
+
+        }
+    }
+}
